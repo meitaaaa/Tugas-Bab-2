8a088740d6d399123dbef629f2f28756b46209d6
meitaaaa <163484689+meitaaaa@users.noreply.github.com>
2024-08-02 12:21:26 +0700

    Add files via upload
    Soal 1 mengenai modifikasi program b5 no 3

---
+++ bab2soal1/src/main/java/com/mycompany/bab2soal1/Bab2soal1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab2soal1;
+
+/**
+ *
+ * @author Wpras
+ */
+public class Bab2soal1 {
+
+    public static void main(String[] args) {
+        //Overflow
+        int a=50;
+        float b=a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
